07fde2a58dfdc8d98f5d64cad170c256ca6b9f19
Your Name <levani.papashvili@labshare.org>
2021-10-05 13:27:29 -0400

    create ItemText

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,14 +4,26 @@ import styled from 'styled-components'
 function Section() {
     return (
         <Wrap>
-            Section
+            <ItemText>
+                <h1>Model S</h1>
+                <p>Order Online for Touchless Delivery</p>
+            </ItemText>
         </Wrap>
     )
 }
 
 export default Section
+
 const Wrap = styled.div`
     width: 100vw;
     height: 100vh;
+    background-size: cover;
+    background-position: center;
+    backgroun-repeat: no-repeat;
     background-image: url('/images/model-s.jpg');
+`
+
+const ItemText = styled.div`
+   padding-top: 15vh;
+   text-align: center;
 `
