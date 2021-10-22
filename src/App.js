6ce9a0e872ea0a0a8ce5de235aff3d5d1771de24
Your Name <levani.papashvili@labshare.org>
2021-10-22 11:19:09 -0400

    UI is done here

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import { Counter } from './features/counter/Counter';
+// import logo from './favicon.ico';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
